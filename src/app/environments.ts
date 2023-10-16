3978fd8568a95ceb66e12023724b0cfcfc181678
Nagy János <janos@zold.lan>
2023-10-16 09:13:53 +0200

    first commit

---
+++ src/app/environments.ts
@@ -0,0 +1,13 @@
+export const Environments = {
+    production:false,
+    firebaseConfig : {
+        apiKey: "AIzaSyA9D_87lnmE2DHy0NgCQLzYQx1LLkpUzb8",
+        authDomain: "dvizsga-3be0a.firebaseapp.com",
+        databaseURL: "https://dvizsga-3be0a-default-rtdb.firebaseio.com",
+        projectId: "dvizsga-3be0a",
+        storageBucket: "dvizsga-3be0a.appspot.com",
+        messagingSenderId: "711034993593",
+        appId: "1:711034993593:web:3442ef0c356666f1839468",
+        measurementId: "G-53354GTB5R"
+      }
+}
